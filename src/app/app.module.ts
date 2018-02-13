880297fb8dbfb870df23633f2c71d0b5c3e3f6d1
chekri <venkatachakradhar@gmail.com>
2018-02-14 01:35:35 +0530

    replicated a layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { AppNavbarComponent } from './app-navbar/app-navbar.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
+import { AppLoaderComponent } from './app-loader/app-loader.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppNavbarComponent,    
+    AppFooterComponent,
+    AppLoaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
