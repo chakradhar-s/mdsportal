7cbc569c7eee481f2cf06b882ec221e9c9f347e3
Manish Kotta <manish.kotta@ggktech.local>
2018-02-14 16:02:09 +0530

    Added lazy loading about component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { Ng4LoadingSpinnerModule } from 'ng4-loading-spinner';
 
 import { AppComponent } from './app.component';
 import { AppNavbarComponent } from './app-navbar/app-navbar.component';
 import { AppFooterComponent } from './app-footer/app-footer.component';
-import { AppLoaderComponent } from './app-loader/app-loader.component';
+import {AboutComponent} from './about/about.component';
+
+
+import { RouterModule, Routes } from '@angular/router';
+import { AppHomeComponent } from './app-home/app-home.component';
+
+const myRoots: Routes = [
+  { path: '', component: AppHomeComponent },
+  {
+    path: 'about',
+    loadChildren: 'app/about/about.module#AboutModule'
+  },
+];
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppNavbarComponent,    
-    AppFooterComponent,
-    AppLoaderComponent
+    AppNavbarComponent,
+    AppFooterComponent, AppHomeComponent
   ],
   imports: [
     BrowserModule,
     NgbModule.forRoot(),
-    Ng4LoadingSpinnerModule.forRoot()
+    Ng4LoadingSpinnerModule.forRoot(),
+    RouterModule.forRoot(myRoots)
   ],
   providers: [],
   bootstrap: [AppComponent]
