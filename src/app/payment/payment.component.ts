4620e56695af102fe5cd8fcbb94a0786345c26f5
Chakradhar Singanamalla <Chakradhar.Singanamalla@ggktech.local>
2018-03-02 23:24:42 +0530

    changed header style

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentComponent implements OnInit {
 
-  private currentJustify = 'start';
+  public currentJustify = 'start';
   constructor() { }
 
   ngOnInit() {
