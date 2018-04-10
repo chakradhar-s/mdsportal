be17c8c3a688a3f98d1dd6108443a9c9531fb287
chakradhar <venkatachakradhar@gmail.com>
2018-04-10 21:58:34 +0530

    file upload image path

--- src/app/upload-questions-image/upload-file/upload-file.component.ts
+++ src/app/upload-questions-image/upload-file/upload-file.component.ts
@@ -10,26 +10,36 @@ import { Alert } from '../../models/alert.interface';
 export class UploadFileComponent implements OnInit {
 
   host_url: string = "http://localhost:5000/mdservice/api/UploadDocument/uploadimages";
-  form_method:string="";
+  form_method: string = "";
   uploadedFiles: any[] = [];
   public alerts: Array<Alert> = [];
 
-  constructor() { }
+  newPaths: string[]=[];
+
+  constructor() {
+
+  }
 
   ngOnInit() {
+    this
   }
 
   onUpload(event) {
-    for(let file of event.files) {
-        this.uploadedFiles.push(file);
+    for (let file of event.files) {
+      this.uploadedFiles.push(file);
     }
-    
+
     this.alerts = [];
-    this.alerts.push({ 
+    this.alerts.push({
       id: 1,
       type: 'success',
-      message:'Upload is successed'
+      message: 'Upload is successed'
     });
-}
+
+    //event.xhr
+    const paths: string[] = JSON.parse(event.xhr.response).pathS;
+    this.newPaths = paths;
+
+  }
 
 }
