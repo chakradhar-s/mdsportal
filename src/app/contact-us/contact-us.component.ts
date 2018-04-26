263fdadf6c05464a716cdf689b565d75451aa674
karthikeya laptop <32848978+karthikei94@users.noreply.github.com>
2018-04-26 09:42:40 +0530

    added required fields in contact us page

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,8 +1,10 @@
 import { FormGroup, FormControl, FormArray, NgForm } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
 
 import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
 import { FormBuilder, Validators } from '@angular/forms';
+import { UserLoginValidators } from '../login-user/login/login-user.validators';
 
 import { CommonService } from '../http-service-registry/services/common.service';
 
@@ -12,41 +14,82 @@ import { CommonService } from '../http-service-registry/services/common.service'
   styleUrls: ['./contact-us.component.scss']
 })
 export class ContactUsComponent implements OnInit {
-  public heroForm: FormGroup;
-  public FirstName:string= '';
-  public LastName:string= '';
-  public Company:string= '';
-  public Email:string= '';
-  public Phone:string= '';
-  public Message:string = '';
-  
+  public submitTriggered:boolean = false;
+  public contactusForm = this.fb.group({
+    FirstName: ['',
+      [Validators.required]],
+    LastName: ['',
+      [Validators.required]],
+    Company: [''],
+    Message: [''],
 
- 
+    Phone: ['',
+      [Validators.required,
+      UserLoginValidators.validMobileNumber]
+    ],
+
+    Email: ['',
+      [Validators.required,
+      UserLoginValidators.validEmailId]
+    ]
+  });
+
+
+
   constructor(
     private spinnerService: Ng4LoadingSpinnerService,
-    private commonService: CommonService
+    private commonService: CommonService,
+    private fb: FormBuilder
   ) { }
 
-  
+
 
   ngOnInit() {
     this.spinnerService.show();
-    this.heroForm = new FormGroup({
-      FirstName: new FormControl(this.FirstName,Validators.required),
-      LastName: new FormControl(this.LastName, Validators.required),
-      Company: new FormControl(this.Company),
-      Email: new FormControl(this.Email),
-      Phone: new FormControl(this.Phone),
-      Message: new FormControl(this.Message)
-    });
   }
 
   onSubmit() {
     debugger;
-    let form = this.heroForm.value;
+    this.submitTriggered = true;
+    let form = this.contactusForm.value;
     this.commonService.GetInTouch(form).subscribe(() => {
-debugger;
-    },(error:Error) => console.log(error));
+      debugger;
+    }, (error: Error) => console.log(error));
+  }
+
+  get emailvalid() {
+    return (
+      this.contactusForm.get('Email').hasError('invalidEmailId') &&
+      this.contactusForm.get('Email').dirty &&
+      !this.required('Email')
+    );
+  }
+
+  required(name: string) {
+    return (
+      this.contactusForm.get(`${name}`).hasError('required') &&
+      this.contactusForm.get(`${name}`).touched
+    );
+  }
+
+  invalid(name: string) {
+    return (
+      this.contactusForm.get(`${name}`).hasError('invalidMobileNumber') &&
+      this.contactusForm.get(`${name}`).dirty &&
+      !this.required(`${name}`)
+    );
+  }
+
+  rebuildForm() {
+    this.submitTriggered = false;
+    this.contactusForm.reset({
+      FirstName: '',
+      LastName: '',
+      Email: '',
+      Phone: '',
+      Company: '',
+      Message: ''
+    });
   }
 
 
