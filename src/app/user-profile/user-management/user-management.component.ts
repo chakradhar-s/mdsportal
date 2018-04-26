fdffa76e59a0382af9c0fd5918041917a92bbef5
chekri <venkatachakradhar@gmail.com>
2018-04-26 07:29:44 +0530

    required field indicators

--- src/app/user-profile/user-management/user-management.component.ts
+++ src/app/user-profile/user-management/user-management.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { UserManagementService } from '../../http-service-registry/services/user-management.service';
 import { LazyLoadEvent } from 'primeng/primeng';
 
+import { Router, ActivatedRoute } from '@angular/router';
+
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { ConfirmationService } from 'primeng/api';
 
@@ -18,11 +20,12 @@ export class UserManagementComponent implements OnInit {
   public cols: any[];
   public loading: boolean;
   public pagesToDisplay: number;
-  public dataTableEvent : DataTableTrackEvent = {currentFilter : '', currentFirstRec: 1, currentRows : 10 };
+  public dataTableEvent: DataTableTrackEvent = { currentFilter: '', currentFirstRec: 1, currentRows: 10 };
   // public loadedEvent : LazyLoadEvent;
   constructor(private manageService: UserManagementService,
-    private confirmationService: ConfirmationService) {
-      debugger;
+    private confirmationService: ConfirmationService,
+    private router: Router) {
+    debugger;
   }
 
   ngOnInit() {
@@ -55,9 +58,9 @@ export class UserManagementComponent implements OnInit {
     this.loading = true;
     console.log(event);
     console.log(this.selectedUsers);
-    this.dataTableEvent={currentFirstRec : event.first + 1, currentRows: event.rows, currentFilter :  event.globalFilter};
+    this.dataTableEvent = { currentFirstRec: event.first + 1, currentRows: event.rows, currentFilter: event.globalFilter };
     this.serviceCall();
-    
+
     // this.loadedEvent = event;
     //in a real application, make a remote request to load data using state metadata from event
     //event.first = First row offset
@@ -67,32 +70,32 @@ export class UserManagementComponent implements OnInit {
     //filters: FilterMetadata object having field as key and filter value, filter matchMode as value
 
     //imitate db connection over a network
- 
-      // if (this.datasource) {
-      //   this.users = this.datasource.slice(event.first, (event.first + event.rows));
-      //   this.loading = false;
-      // }
-      // this.manageService.getUsers(this.dataTableEvent.currentFirstRec, this.dataTableEvent.currentRows,this.dataTableEvent.currentFilter)
-      // .subscribe((response: UserPaginated) => {
-      //   console.log(response);
-      //   if (response) {
-      //     this.users = response.users;
 
-      //     this.totalRecords = response.count;
-      //     if (response.count %this.dataTableEvent.currentRows == 0)
-      //       this.pagesToDisplay = response.count /this.dataTableEvent.currentRows;
-      //     else
-      //       this.pagesToDisplay = (response.count / this.dataTableEvent.currentRows) + 1;
-      //   }
-      //   this.loading = false;
+    // if (this.datasource) {
+    //   this.users = this.datasource.slice(event.first, (event.first + event.rows));
+    //   this.loading = false;
+    // }
+    // this.manageService.getUsers(this.dataTableEvent.currentFirstRec, this.dataTableEvent.currentRows,this.dataTableEvent.currentFilter)
+    // .subscribe((response: UserPaginated) => {
+    //   console.log(response);
+    //   if (response) {
+    //     this.users = response.users;
 
-      // });
-    
+    //     this.totalRecords = response.count;
+    //     if (response.count %this.dataTableEvent.currentRows == 0)
+    //       this.pagesToDisplay = response.count /this.dataTableEvent.currentRows;
+    //     else
+    //       this.pagesToDisplay = (response.count / this.dataTableEvent.currentRows) + 1;
+    //   }
+    //   this.loading = false;
+
+    // });
+
 
   }
 
-  serviceCall(){
-    this.manageService.getUsers(this.dataTableEvent.currentFirstRec, this.dataTableEvent.currentRows,this.dataTableEvent.currentFilter)
+  serviceCall() {
+    this.manageService.getUsers(this.dataTableEvent.currentFirstRec, this.dataTableEvent.currentRows, this.dataTableEvent.currentFilter)
       .subscribe((response: UserPaginated) => {
         console.log(response);
         if (response) {
@@ -100,7 +103,7 @@ export class UserManagementComponent implements OnInit {
 
           this.totalRecords = response.count;
           if (response.count % this.dataTableEvent.currentRows == 0)
-            this.pagesToDisplay = response.count /this.dataTableEvent.currentRows;
+            this.pagesToDisplay = response.count / this.dataTableEvent.currentRows;
           else
             this.pagesToDisplay = (response.count / this.dataTableEvent.currentRows) + 1;
         }
@@ -138,6 +141,10 @@ export class UserManagementComponent implements OnInit {
     });
   }
 
+  registerNav() {
+    this.router.navigate(['/register'], { replaceUrl: true });
+  }
+
 }
 
 export interface UserPaginated {
@@ -145,9 +152,9 @@ export interface UserPaginated {
   users: any[];
 }
 
-export interface DataTableTrackEvent{
-  currentFirstRec : number;
-  currentRows : number;
-  currentFilter : any;
+export interface DataTableTrackEvent {
+  currentFirstRec: number;
+  currentRows: number;
+  currentFilter: any;
 }
 
