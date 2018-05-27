405fa2b6eaf7743628ec8741a6eb13c34525de09
chekri <venkatachakradhar@gmail.com>
2018-05-27 07:54:15 +0530

    changes in results & touchup 1

--- src/app/testimonials/result/result.component.ts
+++ src/app/testimonials/result/result.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
+import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'testimonial',
   templateUrl: './result.component.html',
-  styleUrls: ['./result.component.scss']
+  styleUrls: ['./result.component.scss'],
+  providers: [NgbCarouselConfig]
 })
 export class ResultComponent implements OnInit {
 
-  constructor(private spinnerService: Ng4LoadingSpinnerService) { }
+  constructor(private spinnerService: Ng4LoadingSpinnerService,config: NgbCarouselConfig) {
+    // customize default values of carousels used by this component tree
+    config.interval = 1000;
+    config.wrap = true;
+    config.keyboard = false;
+  }
 
   ngOnInit() {
     this.spinnerService.show();
--- src/app/testimonials/testimonials.module.ts
+++ src/app/testimonials/testimonials.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { TestimonialsRoutingModule } from './testimonials-routing.module';
 import { ResultComponent } from './result/result.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    TestimonialsRoutingModule
+    TestimonialsRoutingModule,
+    NgbModule
   ],
   declarations: [ResultComponent],
   exports: [ResultComponent]
